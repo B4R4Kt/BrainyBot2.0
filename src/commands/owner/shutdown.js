8ffd169e1a53523afdb663664189e351fb75dd90
ahmedmokeddem <63117505+ahmedmokeddem@users.noreply.github.com>
2022-02-05 02:40:34 +0100

    FIX: Set permissions for co-manager and moderator roles

--- src/commands/owner/shutdown.js
+++ src/commands/owner/shutdown.js
@@ -1,15 +1,14 @@
-const {OWNERS} = require('config.json');
-
+const { AuthorizationError } = require('../../core/errors');
 module.exports = {
-  name: 'shutdown',
-  description: 'Shutting down. Bye! :wave:',
-  options: null,
-  execute: async (client, interaction, args) => {
-    if (OWNERS.includes(interaction.user.id)) {
-      await interaction.reply('Shutting down. Bye! :wave:');
-      await interaction.client.destroy();
-    } else {
-      await interaction.reply('Sorry you have not access for that!');
-    }
-  },
+    name: 'shutdown',
+    description: 'Shutting down. Bye! :wave:',
+    options: null,
+    execute: async(client, interaction, args) => {
+        if (interaction.member.roles.cache.some((role) => role.name === 'Co-Manager' || role.name === 'MODERATOR')) {
+            await interaction.reply('Shutting down. Bye! :wave:');
+            await interaction.client.destroy();
+        } else {
+            await new AuthorizationError(interaction);
+        }
+    },
 };
