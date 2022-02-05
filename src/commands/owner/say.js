c193a234ccb88a9f7a810c37f865b0a45e49a1d8
ahmedmokeddem <63117505+ahmedmokeddem@users.noreply.github.com>
2022-02-05 02:40:09 +0100

    FIX: Set permissions for co-manager and moderator roles

--- src/commands/owner/say.js
+++ src/commands/owner/say.js
@@ -1,22 +1,19 @@
-const {OWNERS} = require('config.json');
-
+const { AuthorizationError } = require('../../core/errors');
 module.exports = {
-  name: 'say',
-  description: 'The bot will say anything you want.',
-  options: [
-    {
-      name: 'args',
-      description: 'say anything',
-      type: 'STRING',
-      required: true,
+    name: 'say',
+    description: 'The bot will say anything you want.',
+    options: [{
+        name: 'args',
+        description: 'say anything',
+        type: 'STRING',
+        required: true,
+    }],
+    execute: async(client, interaction, args) => {
+        args = interaction.options.getString('args');
+        if (interaction.member.roles.cache.some((role) => role.name === 'Co-Manager' || role.name === 'MODERATOR')) {
+            await interaction.reply(args);
+        } else {
+            await new AuthorizationError(interaction);
+        }
     },
-  ],
-  execute: async (client, interaction, args) => {
-    args = interaction.options.getString('args');
-    if (OWNERS.includes(interaction.user.id)) {
-      await interaction.reply(args);
-    } else {
-      await interaction.reply('Sorry you have not access for that!');
-    }
-  },
 };
