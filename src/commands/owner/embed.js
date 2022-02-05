7fccab9661d16375983894b3407601a5870d18a0
ahmedmokeddem <63117505+ahmedmokeddem@users.noreply.github.com>
2022-02-05 02:39:20 +0100

    FIX: Set permissions for co-manager and moderator roles

--- src/commands/owner/embed.js
+++ src/commands/owner/embed.js
@@ -1,26 +1,25 @@
-const {MessageEmbed} = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
-const {OWNERS} = require('config.json');
-
+const { AuthorizationError } = require('../../core/errors');
 module.exports = {
-  name: 'embed',
-  description: 'The bot will say anything you want, but within embeds.',
-  options: [{
-    name: 'args',
-    description: 'say anything',
-    type: 'STRING',
-    required: true,
-  }],
-  execute: async (client, interaction, args) => {
-    args = interaction.options.getString('args');
-    if (OWNERS.includes(interaction.user.id)) {
-      const EmbededMessage = new MessageEmbed()
-          .setColor('0x00FF00')
-          .setTitle(args);
-      // replying with the Embeded message
-      await interaction.reply({embeds: [EmbededMessage]});
-    } else {
-      await interaction.reply('Sorry you have not access for that!');
-    }
-  },
+    name: 'embed',
+    description: 'The bot will say anything you want, but within embeds.',
+    options: [{
+        name: 'args',
+        description: 'say anything',
+        type: 'STRING',
+        required: true,
+    }],
+    execute: async(client, interaction, args) => {
+        args = interaction.options.getString('args');
+        if (interaction.member.roles.cache.some((role) => role.name === 'Co-Manager' || role.name === 'MODERATOR')) {
+            const EmbededMessage = new MessageEmbed()
+                .setColor('0x00FF00')
+                .setTitle(args);
+            // replying with the Embeded message
+            await interaction.reply({ embeds: [EmbededMessage] });
+        } else {
+            await new AuthorizationError(interaction);
+        }
+    },
 };
